2168f2982201e0d66a2bca284eefe2fcedbe5b7a
belocer <belocerkovecden@gmail.com>
2017-06-04 11:11:47 +0300

    Помоги пожалуйста два console.log 88стр 89стр и следующая строка идёт сравнение почему оно не срабатывает?

--- src/cookie.js
+++ src/cookie.js
@@ -39,8 +39,6 @@ let addValueInput = homeworkContainer.querySelector('#add-value-input');
 let addButton = homeworkContainer.querySelector('#add-button');
 let listTable = homeworkContainer.querySelector('#list-table tbody');
 
-/*Перебрасываю куки в объект*/
-//console.log(getCookies());
 function getCookies() {
     return document.cookie
         .split('; ')
@@ -53,15 +51,13 @@ function getCookies() {
         }, {});
 }
 
-/*Вывожу на страницу все куки*/
 var cookie_obj = getCookies();
 for (var key in cookie_obj) {
     var td_Tr = document.createElement("tr");
-    td_Tr.innerHTML = "<td>" + key + "</td><td>" + cookie_obj[key] + "</td><td><button class='del' data-key='" + key + "'>Удалить</button></td>";
+    td_Tr.innerHTML = "<td class='first_td'>" + key + "</td><td>" + cookie_obj[key] + "</td><td><button class='del' data-key='" + key + "'>Удалить</button></td>";
     listTable.appendChild(td_Tr);
 }
 
-/*Обработка клика по кнопке + Удаление cookie*/
 var del_cookie = document.querySelectorAll('.del');
 for (var i = 0; i < del_cookie.length; i++) {
     del_cookie[i].addEventListener('click', (e) => {
@@ -73,30 +69,35 @@ for (var i = 0; i < del_cookie.length; i++) {
 }
 
 filterNameInput.addEventListener('keyup', function () {
-
 });
 
 addButton.addEventListener('click', () => {
-    //var p = new Promise(function (resolve, reject) {
- /*       var fil = cookie_obj.every(function(e){
-           return e;
-        });*/
-    //        if (key === addNameInput.value) {
-    //            document.cookie = `${key}=${addValueInput.value}`;
-    //            reject();
-    //        }
-    //    }
 
-    //});
+    var i = 0;
 
-
-    document.cookie = `${addNameInput.value}=${addValueInput.value}`;
-   // if (key === addNameInput.value) {
-
-   // } else {
-        // Если имена не совпадают
+    for (var key in cookie_obj) {
+        if (addNameInput.value == key) {
+            document.cookie = `${key}=${addValueInput.value}`;
+            i++;
+        }
+    }
+    if (i != 0) {
+        var first_td = document.querySelectorAll('.first_td');
+        // console.log(first_td);
+        for (var s = 0; s < first_td.length; s++) {
+            console.log(first_td[s].firstChild);
+            console.log(addNameInput.value);
+            if (first_td[s].firstChild == addNameInput.value) {
+                console.log(111111111111111);
+                first_td[s].firstChild.style.color = "#f00";
+                first_td[s].firstChild.style.background = "#0f0";
+                first_td[s].nextElementSibling.innerHTML = addValueInput.value;
+            }
+        }
+    } else if (i == 0) {
+        document.cookie = `${addNameInput.value}=${addValueInput.value}`;
         var td_Tr = document.createElement("tr");
-        td_Tr.innerHTML = "<td>" + addNameInput.value + "</td><td>" + addValueInput.value + "</td><td><button class='del' data-key='" + addNameInput.value + "'>Удалить</button></td>";
+        td_Tr.innerHTML = "<td class='first_td'>" + addNameInput.value + "</td><td>" + addValueInput.value + "</td><td><button class='del' data-key='" + addNameInput.value + "'>Удалить</button></td>";
         listTable.appendChild(td_Tr);
-    //}
+    }
 });
